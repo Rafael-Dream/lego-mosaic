798c228a621f00f4dd78e80ccf96cdf493f699ba
Rafael.Yu <rafael.yu@advantech.com.tw>
2022-09-07 09:00:22 +0800

    * Try to optimize code, split into functions. * Change tiles directory, prepare for making mosaic image and manual image. + Pure color image, fake green background image. Try to calculate using tile more precisely. + Print result used tiles summary in JSON.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 import sys
+import json
 import glob
 from PIL import Image
 from scipy import spatial
 import numpy as np
+import tile_31204
 
-before_start_let_do_some_calcuation = """
+before_start_lets_do_some_calcuation = """
 Source Lego set is 31204 Elvis Presley.
 Contains 9 pc 16 x 16, which means width: 48, height: 48 (in Lego unit)
 
@@ -14,77 +16,87 @@ Every tile image is 30 x 30 pixels, so output image size will be 1440 x 1440 pix
 Notice: source image need to be squre
 """
 
-main_photo_path = './image/test_1440-2.jpg'
-tiles_img_path = './tile_pool/31204_Elvis/*.jpg'
-tile_size = (30, 30)
-outputHead, outputTail = os.path.splitext(main_photo_path)
-output_path = f"{outputHead}.legoize{outputTail}"
+def create_mosaic(main_photo_path: str, tiles_img_path: str):
+    tile_size = (30, 30)
+    outputHead, outputTail = os.path.splitext(main_photo_path)
+    output_path = f"{outputHead}.legoize{outputTail}"
 
-# Get all tiles
-tile_paths = []
-for file in glob.glob(tiles_img_path):
-    tile_paths.append(file)
-
-
-# Import and resize all tiles
-tiles = []
-for tile_path in tile_paths:
-    tile = Image.open(tile_path)
-    # tile = tile.resize(tile_size)     # Tile image should be 30 x 30 px
-    tiles.append(tile)
+    # Get all tiles
+    tile_paths = []
+    for file in glob.glob(tiles_img_path):
+        tile_paths.append(file)
 
 
-# Calculate dominant color
-colors = []
-for tile in tiles:
-    mean_color = np.array(tile).mean(axis=0).mean(axis=0)
-    colors.append(mean_color)
+    # Import and resize all tiles
+    tiles = []
+    for tile_path in tile_paths:
+        tile = Image.open(tile_path)
+        # tile = tile.resize(tile_size)     # Tile image should be 30 x 30 px
+        tiles.append(tile)
 
 
-# Pixelate (resize) main photo
-main_photo = Image.open(main_photo_path)
-
-width = int(np.round(main_photo.size[0] / tile_size[0]))
-height = int(np.round(main_photo.size[1] / tile_size[1]))
-
-resized_photo = main_photo.resize((width, height))
-
-# Find closest tile photo for every pixel
-tree = spatial.KDTree(colors)
-closest_tiles = np.zeros((width, height), dtype=np.uint32)
-
-for i in range(width):
-    for j in range(height):
-        closest = tree.query(resized_photo.getpixel((i, j)))
-        closest_tiles[i, j] = closest[1]
+    # Calculate dominant color
+    colors = []
+    for tile in tiles:
+        mean_color = np.array(tile).mean(axis=0).mean(axis=0)
+        colors.append(mean_color)
 
 
-# Create an output image
-outputImg = Image.new('RGB', main_photo.size)
+    # Pixelate (resize) main photo
+    main_photo = Image.open(main_photo_path)
 
-# Count tiles
-tileCount = []
-for tile_path in tile_paths:
-    tileDir, tileFileName = os.path.split(tile_path)
-    tileName, tileExt = os.path.splitext(tileFileName)
-    count = [tileName, 0]
-    tileCount.append(count)
+    width = int(np.round(main_photo.size[0] / tile_size[0]))
+    height = int(np.round(main_photo.size[1] / tile_size[1]))
 
-# Draw tiles
-for i in range(width):
-    for j in range(height):
-        # Offset of tile
-        x, y = i*tile_size[0], j*tile_size[1]
-        # Index of tile
-        index = closest_tiles[i, j]
-        # Draw tile
-        outputImg.paste(tiles[index], (x, y))
-        # Count tile
-        tileCount[index][1] += 1
+    resized_photo = main_photo.resize((width, height))
 
-# Save output
-if os.path.exists(output_path):
-    os.remove(output_path)
-outputImg.save(output_path)
+    # Find closest tile photo for every pixel
+    tree = spatial.KDTree(colors)
+    closest_tiles = np.zeros((width, height), dtype=np.uint32)
 
-print(tileCount)
+    for i in range(width):
+        for j in range(height):
+            closest = tree.query(resized_photo.getpixel((i, j)))
+            closest_tiles[i, j] = closest[1]
+
+
+    # Create an output image
+    outputImg = Image.new('RGB', main_photo.size)
+
+    # Count tiles
+    tileCount = []
+    for tile_path in tile_paths:
+        tileDir, tileFileName = os.path.split(tile_path)
+        tileName, tileExt = os.path.splitext(tileFileName)
+        count = [tileName, 0]
+        tileCount.append(count)
+
+    # Draw tiles
+    for i in range(width):
+        for j in range(height):
+            # Offset of tile
+            x, y = i*tile_size[0], j*tile_size[1]
+            # Index of tile
+            index = closest_tiles[i, j]
+            # Draw tile
+            outputImg.paste(tiles[index], (x, y))
+            # Count tile
+            tileCount[index][1] += 1
+
+    # Save output
+    if os.path.exists(output_path):
+        os.remove(output_path)
+    outputImg.save(output_path)
+
+    outputSummary(tileCount)
+
+def outputSummary(tileCount):
+    summary = {}
+    for count in tileCount:
+        summary[count[0]] = count[1]
+
+    print(json.dumps(summary, indent=2))
+
+
+main_photo_path = './image/test_1440-3.jpg'
+create_mosaic(main_photo_path, tile_31204.tileColorPath)
---
+++ tile_31204.py
@@ -0,0 +1,27 @@
+name = 'LEGO 31204 Elvis Presley'
+tileColorPath = './tile_pool/31204_Elvis/color/*.jpg'
+tileManualPath = './tile_pool/31204_Elvis/manual/*.jpg'
+# "{Index Number In Build Instruction}-{LEGO Brick ID}": Tile Count in LEGO set
+# For example: "01-6284070": 596
+# Index Number In Build Instruction: 01
+# LEGO Brick ID: 6284070
+# Tile Count: 596, so there are 596 tiles in maximum
+tileCount = {
+  "01-6284070": 596,
+  "02-6284584": 152,
+  "03-6284602": 230,
+  "04-6322842": 175,
+  "05-6284596": 52,
+  "06-6284071": 26,
+  "07-6284572": 71,
+  "08-6284573": 106,
+  "09-6315196": 360,
+  "10-6343472": 258,
+  "11-6284589": 182,
+  "12-6322840": 179,
+  "13-6284586": 233,
+  "14-6322813": 206,
+  "15-6284585": 339,
+  "16-6284574": 80,
+  "99-BG00FF21": 0
+}
